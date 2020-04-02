bb822767f7d66b5409a3ddfe5c137315425cf862
SirHesham <heshammmchess@gmail.com>
2020-04-02 19:50:29 +0200

    Product Resource

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('/products', 'ProductController@');
+
+Route::apiResource('/products', 'ProductController');
+
 Route::group(['prefix' => 'products'], function () {
     Route::apiResource('/{product}/reviews', 'ReviewController');
 });
